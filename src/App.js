9a9be58b61782156a00a7a6da0c728d391594972
piyush016 <piyushcool116@gmail.com>
2023-09-11 23:26:35 +0530

    Addition of packages, image and basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import CartProvider from "./store/CartProvider";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
